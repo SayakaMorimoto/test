af10fadcba91e03e4520712f5213c781c86555f5
Ryokada <ryokada.5088@gmail.com>
2017-06-09 00:42:39 +0900

    Lチカプログラムの追加

---
+++ l_chika/l_chika.ino
@@ -0,0 +1,14 @@
+int outputPin = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(outputPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(outputPin, HIGH);
+  delay(1000);
+  digitalWrite(outputPin, LOW);
+  delay(1000);
+}
